79597fd9b143f3493422a47a9178e0bcfa5c426d
David Schaller <38377210+david-schaller@users.noreply.github.com>
2021-01-27 13:57:55 +0100

    docstrings

--- src/asymmetree/datastructures/Tree.py
+++ src/asymmetree/datastructures/Tree.py
@@ -544,7 +544,23 @@ class Tree:
         
     
     def random_leaves(self, proportion):
-        """Return a random subset of the leaves."""
+        """A random sample of the leaves.
+        
+        Parameters
+        ----------
+        proportion : float
+            The proportion of the sample w.r.t. the full set of leaves.
+        
+        Returns
+        -------
+        list of TreeNode objects
+            A random sample of the leaves of the tree.
+        
+        Raises
+        ------
+        ValueError
+            If `proportion` is not a number between 0 and 1. 
+        """
         
         if (not isinstance(proportion, (float, int)) or 
             proportion < 0 or proportion > 1):
@@ -557,7 +573,18 @@ class Tree:
       
     
     def get_hierarchy(self):
-        """Return the hierarchy set on the leaf labels defined by the tree."""
+        """Hierarchy set on the leaf labels defined by the tree.
+        
+        Every (phylogenetic) tree can be represented by a hierarchy on the set
+        of its leaves.
+        The labels of the leaf nodes must be unique.
+        
+        Returns
+        -------
+        set of lists of str objects
+            Representing the hierarchy where the leaves are represented by
+            their labels.
+        """
         
         self.supply_leaves()
         
@@ -574,9 +601,20 @@ class Tree:
     
     
     def compare_topology(self, other):
-        """Compare the tree topology based on the hierarchies.
+        """Compare the tree topology based on the leaf labels.
         
-        Only works for phylogenetic trees."""
+        Only works for phylogenetic trees with unique leaf labels.
+        
+        Parameters
+        ----------
+        other : Tree
+            The tree which this tree is compared to.
+        
+        Returns
+        -------
+        bool
+            True if the topologies are equal, else False.
+        """
         
         hierarchy1 = sorted(self.get_hierarchy())
         hierarchy2 = sorted(other.get_hierarchy())
@@ -611,14 +649,28 @@ class Tree:
     
     @staticmethod
     def random_tree(N, binary=False):
-        """Creates a random tree.
+        """A random tree.
         
-        The number of leaves is specified by the parameter 'N'. Each non-leaf
-        node in the resulting tree will have at least children (property of
-        phylogenetic trees).
+        The resulting tree is always phylogenetic, i.e., each inner node has
+        at least two children.
         
-        Keyword arguments:
-            binary - forces the tree to be binary; default is False
+        Parameters
+        ----------
+        N : int
+            The desired number of leaves.
+        binary : bool
+            If True, the resulting tree is binary, otherwise it may contain
+            multifurcations.
+        
+        Returns
+        -------
+        Tree
+            A randomly generated tree with `N` leaves.
+            
+        Raises
+        ------
+        TypeError
+            If `N` is not an integer >= 1.
         """
         
         if not (isinstance(N, int)) or N < 1:
@@ -661,7 +713,7 @@ class LCA:
     table implementation.
     Preprocessing complexity: O(n * log n)
     Query complexity: O(1)
-    where n is the number of vertices in the tree.
+    where n is the number of nodes in the tree.
     
     References
     ----------
@@ -673,31 +725,44 @@ class LCA:
     """
     
     def __init__(self, tree):
+        """Constructor for class LCA.
+        
+        Parameters
+        ----------
+        tree : Tree
+            The Tree instance for which this instance will allow efficient
+            last common ancestor queries.
+        
+        Raises
+        ------
+        TypeError
+            If `tree` is not a Tree instance.
+        """
         
         if not isinstance(tree, Tree):
             raise TypeError("tree must be of type 'Tree'")
             
-        self.tree = tree
+        self._tree = tree
         
-        self.V = [v for v in self.tree.preorder()]
-        self.index = {v: i for i, v in enumerate(self.V)}
+        self._V = [v for v in self._tree.preorder()]
+        self._index = {v: i for i, v in enumerate(self._V)}
         
         # store IDs for queries via ID
-        self.id_dict = {v.ID: v for v in self.V}
+        self._id_dict = {v.ID: v for v in self._V}
         
-        self.euler_tour = []
+        self._euler_tour = []
         # levels of the vertices in the Euler tour
-        self.L = []
+        self._L = []
         
-        for v, level in self.tree.euler_and_level():
-            self.euler_tour.append(self.index[v])
-            self.L.append(level)
+        for v, level in self._tree.euler_and_level():
+            self._euler_tour.append(self._index[v])
+            self._L.append(level)
         
         # repres. of vertices in the Euler tour (index of first occurence)
-        self.R = [None for _ in range(len(self.V))]
-        for j, i in enumerate(self.euler_tour):
-            if self.R[i] is None:
-                self.R[i] = j
+        self._R = [None for _ in range(len(self._V))]
+        for j, i in enumerate(self._euler_tour):
+            if self._R[i] is None:
+                self._R[i] = j
                 
         # build sparse table for range minimum query (RMQ)
         self._precompute_logs()
@@ -705,7 +770,20 @@ class LCA:
         
         
     def __call__(self, a, b):
-        """Return the last common ancestor of two nodes."""
+        """Last common ancestor of two nodes.
+        
+        Parameters
+        ----------
+        a : TreeNode or int
+            A node or its ID in the tree corresponding to this LCA instance.
+        b : TreeNode or int
+            A node or its ID in the tree corresponding to this LCA instance.
+        
+        Returns
+        -------
+        TreeNode
+            The last common ancestor of `a` and `b`.
+        """
         
         return self._get_lca(self._id_to_treenode(a),
                              self._id_to_treenode(b))
@@ -713,14 +791,42 @@ class LCA:
         
     
     def get(self, a, b):
-        """Return the last common ancestor of two nodes."""
+        """Last common ancestor of two nodes.
+        
+        Parameters
+        ----------
+        a : TreeNode or int
+            A node or its ID in the tree corresponding to this LCA instance.
+        b : TreeNode or int
+            A node or its ID in the tree corresponding to this LCA instance.
+        
+        Returns
+        -------
+        TreeNode
+            The last common ancestor of `a` and `b`.
+        """
         
         return self._get_lca(self._id_to_treenode(a),
                              self._id_to_treenode(b))
     
     
     def displays_triple(self, a, b, c):
-        """Return whether the tree displays the rooted triple ab|c (= ba|c)."""
+        """Return whether the tree displays the rooted triple ab|c (= ba|c).
+        
+        Parameters
+        ----------
+        a : TreeNode or int
+            A node or its ID in the tree corresponding to this LCA instance.
+        b : TreeNode or int
+            A node or its ID in the tree corresponding to this LCA instance.
+        c : TreeNode or int
+            A node or its ID in the tree corresponding to this LCA instance.
+        
+        Returns
+        -------
+        bool
+            True if the tree displays the triple ab|c (= ba|c).
+        """
         
         try:
             return self._has_triple(self._id_to_treenode(a),
@@ -731,22 +837,63 @@ class LCA:
         
     
     def are_comparable(self, u, v):
-        """Return whether two nodes/edges are comparable w.r.t. ancestor
-        relation."""
+        """Returns True if two nodes/edges are comparable in the tree.
+        
+        Two nodes/edges are comparable if one lies on the unique path from the
+        other to the root of the tree.
+        
+        Parameters
+        ----------
+        u : TreeNode or int or tuple of two TreeNode or int objects
+            An node or edge in the tree corresponding to this LCA instance.
+        v : TreeNode or int or tuple of two TreeNode or int objects
+            An node or edge in the tree corresponding to this LCA instance.
+            
+        Return
+        ------
+        bool
+            True if `u` and `v` are comparable in the tree, else False.
+        """
         
         return self._are_comparable(self._id_to_treenode(u),
                                     self._id_to_treenode(v))
     
     
     def ancestor_or_equal(self, u, v):
-        """Return whether u is equal to or an ancestor of v."""
+        """Return True if u is equal to or an ancestor of v.
+        
+        Parameters
+        ----------
+        u : TreeNode or int or tuple of two TreeNode or int objects
+            An node or edge in the tree corresponding to this LCA instance.
+        v : TreeNode or int or tuple of two TreeNode or int objects
+            An node or edge in the tree corresponding to this LCA instance.
+            
+        Return
+        ------
+        bool
+            True if `u` is equal or an ancestor of `v`, else False.
+        """
         
         return self._ancestor_or_equal(self._id_to_treenode(u),
                                        self._id_to_treenode(v))
     
     
     def ancestor_not_equal(self, u, v):
-        """Return whether u is a strict ancestor of v."""
+        """Return True if u is a strict ancestor of v.
+        
+        Parameters
+        ----------
+        u : TreeNode or int or tuple of two TreeNode or int objects
+            An node or edge in the tree corresponding to this LCA instance.
+        v : TreeNode or int or tuple of two TreeNode or int objects
+            An node or edge in the tree corresponding to this LCA instance.
+            
+        Return
+        ------
+        bool
+            True if `u` is a strict ancestor of `v`, else False.
+        """
         
         u = self._id_to_treenode(u)
         v = self._id_to_treenode(v)
@@ -755,25 +902,74 @@ class LCA:
     
     
     def descendant_or_equal(self, u, v):
-        """Return whether u is equal to or a descendant of v."""
+        """Return True if u is equal to or a descendant of v.
+        
+        Parameters
+        ----------
+        u : TreeNode or int or tuple of two TreeNode or int objects
+            An node or edge in the tree corresponding to this LCA instance.
+        v : TreeNode or int or tuple of two TreeNode or int objects
+            An node or edge in the tree corresponding to this LCA instance.
+            
+        Return
+        ------
+        bool
+            True if `u` is equal or a descendant of `v`, else False.
+        """
         
         return self.ancestor_or_equal(v, u)
     
     
     def descendant_not_equal(self, u, v):
-        """Return whether u is a strict descendant of v."""
+        """Return True if u is a strict descendant of v.
+        
+        Parameters
+        ----------
+        u : TreeNode or int or tuple of two TreeNode or int objects
+            An node or edge in the tree corresponding to this LCA instance.
+        v : TreeNode or int or tuple of two TreeNode or int objects
+            An node or edge in the tree corresponding to this LCA instance.
+            
+        Return
+        ------
+        bool
+            True if `u` is a strict descendant of `v`, else False.
+        """
         
         return self.ancestor_not_equal(v, u)
     
     
     def consistent_triples(self, triples):
-        """Return a list with the subset of 'triples' that are displayed."""
+        """List with the subset of triples that are displayed by the tree.
+        
+        Parameters
+        ----------
+        triples : an iterable object of tuples of three TreeNode or int objects
+            Input triples of which each may or may not be displayed by the tree.
+        
+        Returns
+        -------
+        list of tuples of three TreeNode of int objects
+            Representing the subset of the input list that are displayed by the
+            tree.
+        """
         
         return [t for t in triples if self.displays_triple(*t)]
     
     
     def consistent_triple_generator(self, triples):
-        """Generator for the items in 'triples' that are displayed."""
+        """Generator for the items in 'triples' that are displayed.
+        
+        Parameters
+        ----------
+        triples : an iterable object of tuples of three TreeNode or int objects
+            Input triples of which each may or may not be displayed by the tree.
+        
+        Yields
+        -------
+        tuple of three TreeNode of int objects
+            For each triple in the input list that is displayed by the tree.
+        """
         
         for t in triples:
             if self.displays_triple(*t):
@@ -782,7 +978,7 @@ class LCA:
     
     def _precompute_logs(self):
         
-        n = len(self.L)
+        n = len(self._L)
         self.log = [0 for _ in range(n + 1)]
         for i in range(2, n + 1):
             self.log[i] = int(self.log[i//2]) + 1
@@ -790,15 +986,15 @@ class LCA:
         
     def _RMQ_sparse_table(self):
         
-        n = len(self.L)
+        n = len(self._L)
         K = self.log[n]
         
         # lookup table M
-        self.M = [[0 for j in range(K + 1)] for i in range(n)]
+        self._M = [[0 for j in range(K + 1)] for i in range(n)]
         
         # initialize the intervals with length 1
         for i in range(n):
-            self.M[i][0] = i
+            self._M[i][0] = i
          
         # dynamic programming: compute values from smaller to bigger intervals  
         for j in range(1, K + 1):
@@ -806,20 +1002,20 @@ class LCA:
             # compute minimum value for all intervals with size 2^j
             for i in range(n - (1 << j) + 1):
                 
-                if (self.L[ self.M[i][j - 1] ] <
-                    self.L[ self.M[i + (1 << (j - 1))][j - 1] ]):
-                    self.M[i][j] = self.M[i][j - 1]
+                if (self._L[ self._M[i][j - 1] ] <
+                    self._L[ self._M[i + (1 << (j - 1))][j - 1] ]):
+                    self._M[i][j] = self._M[i][j - 1]
                 else:
-                    self.M[i][j] = self.M[i + (1 << (j - 1))][j - 1]
+                    self._M[i][j] = self._M[i + (1 << (j - 1))][j - 1]
     
     
     def _RMQ_query(self, i, j):  
         
         k = self.log[j - i + 1]
-        if self.L[self.M[i][k]] < self.L[self.M[j - (1 << k) + 1][k]]:
-            return self.M[i][k]
+        if self._L[self._M[i][k]] < self._L[self._M[j - (1 << k) + 1][k]]:
+            return self._M[i][k]
         else:
-            return self.M[j - (1 << k) + 1][k]
+            return self._M[j - (1 << k) + 1][k]
         
     
     def _id_to_treenode(self, v):
@@ -830,7 +1026,7 @@ class LCA:
             return (self._id_to_treenode(v[0]),
                     self._id_to_treenode(v[1]))
         else:
-            return self.id_dict[v]
+            return self._id_dict[v]
         
         
     def _get_lca(self, v1, v2):
@@ -838,15 +1034,17 @@ class LCA:
         if v1 is v2:
             return v1
         
-        r1 = self.R[self.index[v1]]
-        r2 = self.R[self.index[v2]]
+        r1 = self._R[self._index[v1]]
+        r2 = self._R[self._index[v2]]
         if r1 > r2:
             r1, r2 = r2, r1
-        return self.V[ self.euler_tour[self._RMQ_query(r1, r2)] ]
+        return self._V[ self._euler_tour[self._RMQ_query(r1, r2)] ]
         
     
     def _has_triple(self, a, b, c):
         
+        if a is b:
+            return False
         lca_ab = self._get_lca(a, b)
         return lca_ab is not self._get_lca(lca_ab, c)
     
@@ -876,7 +1074,22 @@ class LCA:
         
 
 def lcas_naive(tree):
-    """Naive computation of the last common ancestor for all leaf pairs."""
+    """Naive computation of the last common ancestor for all leaf pairs.
+    
+    .. deprecated:: 1.0.0
+        `lcas_naive` may be removed in future version of AsymmeTree.
+        
+    Parameters
+    ----------
+    tree : Tree
+        The tree for which the last common ancestors are computed.
+    
+    Returns
+    -------
+    dict
+        Keys are all pairs of TreeNode objects that are leaves, and the values
+        correspond to the TreeNode that is their last common ancestor.
+    """
     
     lcas = {}
     tree.supply_leaves()
@@ -887,36 +1100,3 @@ def lcas_naive(tree):
                 lcas[x, y] = u
     
     return lcas
-
-
-if __name__ == '__main__':
-    
-    # import time
-    # tree = Tree.random_tree(4000)
-    # leaves = tree.supply_leaves()
-    
-    # start_time1 = time.time()
-    # lca_fast = LCA(tree)
-    # end_time1 = time.time()
-    
-    # start_time2 = time.time()
-    # lca_naive = lcas_naive(tree)
-    # end_time2 = time.time()
-    
-    # print(end_time1 - start_time1, end_time2 - start_time2)
-    
-    # for i in range(10):
-    #     v1, v2 = random.sample(leaves, 2)
-        
-    #     print(v1, v2, lca_fast.get(v1, v2), lca_naive[v1, v2])
-    
-    tree = Tree.random_tree(10)
-    print(tree.to_newick())
-    leaves = tree.supply_leaves()
-    lca = LCA(tree)
-    
-    for i in range(10):
-        a, b, c = random.sample(leaves, 3)
-        
-        print(a, b, c, lca.displays_triple(a, b, c),
-                       lca.displays_triple(a.ID, b.ID, c.ID))
