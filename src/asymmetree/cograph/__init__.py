044d2e2700cc5ed321936eb463af328d8292819a
David Schaller <38377210+david-schaller@users.noreply.github.com>
2021-01-29 10:45:41 +0100

    Cluster Deletion for cographs

--- src/asymmetree/cograph/__init__.py
+++ src/asymmetree/cograph/__init__.py
@@ -6,3 +6,6 @@ from asymmetree.cograph.Cograph import (Cotree,
                                         LinearCographDetector)
 
 from asymmetree.cograph.CographEditor import edit_to_cograph, CographEditor
+
+from asymmetree.cograph.ClusterDeletion import (cluster_deletion,
+                                                complete_multipartite_completion)
--- tests/cograph_tests.py
+++ tests/cograph_tests.py
@@ -3,8 +3,10 @@
 import unittest
 import networkx as nx
 
-from asymmetree.cograph import Cotree, linear_cograph_detection, CographEditor
-from asymmetree.tools.GraphTools import random_graph
+from asymmetree.cograph import (Cotree, linear_cograph_detection,
+                                CographEditor,
+                                complete_multipartite_completion)
+import asymmetree.tools.GraphTools as gt
 
 
 __author__ = 'David Schaller'
@@ -41,12 +43,21 @@ class TestCographPackage(unittest.TestCase):
     
     def test_editing(self):
         
-        graph = random_graph(100, p=0.3)
+        graph = gt.random_graph(100, p=0.3)
         ce = CographEditor(graph)
         ce.cograph_edit(run_number=10)
         cograph = ce.best_T.to_cograph()
         
         self.assertTrue( linear_cograph_detection(cograph) )
+    
+    
+    def test_compl_multipart_completion(self):
+        
+        cotree = Cotree.random_cotree(20)
+        sets, cmg = complete_multipartite_completion(cotree, supply_graph=True)
+        orig_cograph =  cotree.to_cograph()
+        
+        self.assertTrue(gt.is_subgraph(orig_cograph, cmg))
         
 
 if __name__ == '__main__':
