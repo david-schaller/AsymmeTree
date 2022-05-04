a5961c9db18892c7827aa1c74401371f7751bca4
David Schaller <38377210+david-schaller@users.noreply.github.com>
2022-05-04 11:46:39 +0200

    create pyproject.toml and setup.cfg

--- setup.py
+++ setup.py
@@ -1,30 +1,7 @@
-import setuptools
+# -*- coding: utf-8 -*-
 
-with open('README.md', 'r') as fh:
-    long_description = fh.read()
+from setuptools import setup
 
-setuptools.setup(
-    name='asymmetree',
-    version='2.2.0',
-    author='David Schaller',
-    author_email='sdavid@bioinf.uni-leipzig.de',
-    description='Simulation of phylogenetic trees, sequences and genomes.',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/david-schaller/AsymmeTree',
-    packages=setuptools.find_packages('src'),
-    package_dir={'': 'src'},
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Operating System :: OS Independent',
-    ],
-    python_requires='>=3.7',
-    install_requires=[
-        'numpy>=1.16.4',
-        'scipy>=1.3.0',
-        'matplotlib>=3.0',
-        'networkx>=2.2',
-        'tralda>=1.0.0',
-   ],
-)
+if __name__ == '__main__':
+    
+    setup()
